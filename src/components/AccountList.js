e88b53fdf351243a83ad16fd6e0048197b003bbc
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-01-31 16:37:50 +0530

    Refactor swipe card and use single component

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -1,43 +1,49 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 
+const AccountItem = props => {
+  return (
+    <View style={styles.listItem}>
+      <Image
+        style={{ width: 50, height: 50, marginVertical: 15 }}
+        source={{
+          uri: props.imageUrl
+        }}
+      />
+      <Text style={styles.accountName}>{props.name}</Text>
+      <Text style={styles.accountBalance}>{props.balance}</Text>
+    </View>
+  );
+};
+
 export default class AccountList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCard: 0
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView horizontal={true}>
-          <View style={styles.listItem}>
-            <Image
-              style={{ width: 50, height: 50, marginVertical: 15 }}
-              source={{
-                uri:
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/SBI-logo.svg/2000px-SBI-logo.svg.png"
-              }}
-            />
-            <Text style={styles.accountName}>SBI</Text>
-            <Text style={styles.accountBalance}>10,000 ₹</Text>
-          </View>
-          <View style={styles.listItem}>
-            <Image
-              style={{ width: 200, height: 50, marginVertical: 15 }}
-              source={{
-                uri: "http://bankifsccodehub.com/logos/Kotak-Mahindra-Bank.png"
-              }}
-            />
-            <Text style={styles.accountName}>Kotak</Text>
-            <Text style={styles.accountBalance}>10,000 ₹</Text>
-          </View>
-          <View style={styles.listItem}>
-            <Image
-              style={{ width: 200, height: 50, marginVertical: 15 }}
-              source={{
-                uri:
-                  "https://i0.wp.com/www.noticebard.com/wp-content/uploads/2018/02/paytm-customer-care-internship-Delhi.png"
-              }}
-            />
-            <Text style={styles.accountName}>Paytm</Text>
-            <Text style={styles.accountBalance}>500 ₹</Text>
-          </View>
+          <AccountItem
+            imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/SBI-logo.svg/2000px-SBI-logo.svg.png"
+            name="SBI"
+            balance="10,000 ₹"
+            currentCard
+          />
+          <AccountItem
+            imageUrl="http://bankifsccodehub.com/logos/Kotak-Mahindra-Bank.png"
+            name="Kotak"
+            balance="10,000 ₹"
+          />
+          <AccountItem
+            imageUrl="https://i0.wp.com/www.noticebard.com/wp-content/uploads/2018/02/paytm-customer-care-internship-Delhi.png"
+            name="Paytm"
+            balance="10,000 ₹"
+          />
         </ScrollView>
       </View>
     );
@@ -54,19 +60,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     padding: 5,
     margin: 10,
-    backgroundColor: "white",
-    height: 150,
-    width: 225,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "tomato"
+    backgroundColor: "#262838",
+    height: 200,
+    width: 300,
+    borderRadius: 10
   },
   accountName: {
     color: "tomato",
     fontSize: 18
   },
   accountBalance: {
-    color: "black",
+    color: "white",
     fontSize: 24
   }
 });
